b499f30c231d602ba13ba8e599633aade46abb10
Andrew Lunde <andrew.lunde@sap.com>
2019-06-05 11:14:11 -0400

    first commit

---
+++ srv/server.js
@@ -0,0 +1,53 @@
+/*eslint no-console: 0, no-unused-vars: 0*/
+"use strict";
+
+const express = require("express");
+
+var app = express();
+
+app.get("/", function (req, res) {
+
+	const bpService = require("@sap/cloud-sdk-vdm-business-partner-service");
+
+	function getAllBusinessPartners() {
+		return bpService.BusinessPartner.requestBuilder()
+			.getAll()
+			.top(10)
+			.select(
+				bpService.BusinessPartner.BUSINESS_PARTNER,
+				bpService.BusinessPartner.FIRST_NAME,
+				bpService.BusinessPartner.LAST_NAME
+			)
+			.filter(
+				bpService.BusinessPartner.FIRST_NAME.equals("John")
+			)
+			/*
+			.withCustomHeaders({
+				"APIKey": "<API_KEY>"
+			})
+			.execute({
+				url: "https://sandbox.api.sap.com/s4hanacloud"
+			*/
+			.execute({
+				destinationName: "S4"
+			})
+	};
+
+	getAllBusinessPartners()
+		.then(results => {
+			res.status(200).json(results);
+		})
+		.catch(error => {
+			res.status(500).send(error.message);
+		});
+
+});
+
+var server = require("http").createServer();
+var port = process.env.PORT || 3000;
+
+server.on("request", app);
+
+server.listen(port, function () {
+	console.info("srv: " + server.address().port);
+});
